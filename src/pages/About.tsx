2334c6410b46f304350e3a9ce42b5bef9e4bd4ca
sanjeet159 <84581508+sanjeet159@users.noreply.github.com>
2026-04-18 14:31:50 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -79,7 +79,7 @@ const About = () => {
                 </Button>
               </Link>
               <Link to="/contact">
-                <Button size="lg" variant="outline" className="border-primary-foreground/20 text-primary-foreground hover:bg-primary-foreground/100 font-semibold px-8 h-12 rounded-full">
+                <Button size="lg" variant="outline" className="border-primary-foreground/20 text-primary-foreground hover:bg-primary-foreground/0 font-semibold px-8 h-12 rounded-full">
                   Get in Touch
                 </Button>
               </Link>
